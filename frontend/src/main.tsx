3de54ab85c22830079c6c3ababcfaf8abd3d7128
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2024-02-09 12:15:49 -0700

    updated prettier config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// Entry file for React
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root') as HTMLElement; // Get the root element of our HTML page
+const root = ReactDOM.createRoot(rootElement); // Create the root component using createRoot() method provided by react-dom/client
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
