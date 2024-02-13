8f1be74124d8ea7b20092cb19f0c59a8b7330902
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2024-02-13 16:34:17 -0700

    Added CORS config to backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,16 @@
-import express from 'express'
-import './db' // import db
-import noteRouter from './routers/note'
+import express from 'express';
+import './db'; // import db
+import noteRouter from './routers/note';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({extended: false})) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+app.use(cors());
 
 app.listen(8000, () => {
-    console.log('Server running on port 8000')
-})
+  console.log('Server running on port 8000');
+});
 
 app.use('/note', noteRouter);
