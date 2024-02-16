fdf7269816e4872f63c690c0f6939297b2abe664
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2024-02-16 09:17:54 -0700

    Added Helmet and Morgan dep

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import './db'; // import db
 import noteRouter from './routers/note';
 import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 app.use(cors()); // enable cors
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.listen(8000, () => {
   console.log('Server running on port 8000');
